b2bf441c60ecac76ba584c2579ab9dba193f052b
Kyosuke <love.frisk1188@gmail.com>
2025-01-08 15:34:26 +0900

    【ログイン画面】パスワードリセット機能を実装

--- src/config/settings.py
+++ src/config/settings.py
@@ -150,3 +150,10 @@ NUMBER_GROUPING = 3
 
 # メッセージストレージ
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
--- src/login/views.py
+++ src/login/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.contrib import messages
+
+
 
 class Login(LoginView):
     fields = '__all__'
@@ -13,3 +15,19 @@ class Login(LoginView):
 
 class Logout(LogoutView):
     template_name = 'login/login.html'
+
+class PasswordReset(PasswordResetView):
+    subject_template_name = 'mail/password_reset/password_reset_subject.txt'
+    email_template_name = 'mail/password_reset/password_reset_email.txt'
+    template_name = 'login/password_reset_form.html'
+    success_url = reverse_lazy('login:password_reset_done')
+
+class PasswordResetDone(PasswordResetDoneView):
+    template_name = 'login/password_reset_done.html'
+
+class PasswordResetConfirm(PasswordResetConfirmView):
+    success_url = reverse_lazy('login:password_reset_complete')
+    template_name = 'login/password_reset_confirm.html'
+
+class PasswordResetComplete(PasswordResetCompleteView):
+    template_name = 'login/password_reset_complete.html'
