89998084127e9b23b6f9b6d24763b386d8c7ce9b
Kyosuke <love.frisk1188@gmail.com>
2025-01-09 15:49:50 +0900

    【アイテムマスタ画面】ページング時の不具合を修正

--- src/demo_app/views/item_mst.py
+++ src/demo_app/views/item_mst.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+import re
 
 
 class ItemList(LoginRequiredMixin, generic.ListView, generic.edit.ModelFormMixin):
@@ -42,13 +43,8 @@ class ItemList(LoginRequiredMixin, generic.ListView, generic.edit.ModelFormMixin
         # カテゴリマスタのデータをフォームに適用
         categories = Category.objects.order_by('id')
         context['category_list'] = categories
-        page: Page = context['page_obj']
         # ページネーション設定
-        context['paginator_range'] = page.paginator.get_elided_page_range(
-            page.number
-            , on_each_side=1
-            , on_ends=1
-        )
+        context = set_pagination(context, self.request.GET.urlencode())
         # フォームの値を設定
         context['form'] = self.get_form
         return context
@@ -168,3 +164,20 @@ class ItemDelete(LoginRequiredMixin, generic.edit.DeleteView):
         )
         # 処理結果を返却
         return result
+    
+def set_pagination(context, url_params):
+    """
+    ページング部品の値をコンテキストに設定
+    """
+    page: Page = context['page_obj']
+    context['paginator_range'] = page.paginator.get_elided_page_range(
+        page.number
+        , on_each_side=1
+        , on_ends=1
+    )
+    # ページングのパラメータを削除
+    url_params = re.sub('\&page\=\d*', '', url_params)
+    # URLパラメータを再設定
+    context['query_str'] = url_params
+    # 処理結果を返却
+    return context
