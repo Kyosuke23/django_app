24596fd17f77b46a8b35cccb213e3099f5d42f2c
Kyosuke <love.frisk1188@gmail.com>
2025-06-19 09:22:41 +0900

    一覧画面に権限による制御を実装

--- src/register/const.py
+++ src/register/const.py
@@ -3,18 +3,28 @@
 """
 
 # 性別
+GENDER_MALE = '0'
+GENDER_FEMALE = '1'
+GENDER_OTHER = '2'
 GENDER_CHOICES = (
     ('', '')
-    , ('0', '男性')
-    , ('1', '女性')
-    , ('2', 'その他')
+    , (GENDER_MALE, '男性')
+    , (GENDER_FEMALE, '女性')
+    , (GENDER_OTHER, 'その他')
 )
+
 # ユーザー権限
+PRIVILEGE_SUPER = '0'
+PRIVILEGE_NORMAL = '1'
+PRIVILEGE_REFERENCE = '2'
+PRIVILEGE_GUEST = '3'
 PRIVILEGE_CHOICES = (
-    ('0', '特権')
-    , ('1', '一般')
-    , ('2', '参照')
+    (PRIVILEGE_SUPER, '特権')
+    , (PRIVILEGE_NORMAL, '一般')
+    , (PRIVILEGE_REFERENCE, '参照')
+    , (PRIVILEGE_GUEST, 'ゲスト')
 )
+
 # 都道府県
 STATE_CHOICES = (
     ('', ''),
