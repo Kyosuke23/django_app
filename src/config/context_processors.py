88713567f85fe1de6c927544c6d87fe17f33021a
Kyosuke <love.frisk1188@gmail.com>
2025-06-23 18:07:21 +0900

    一部の定数をグローバル化

---
+++ src/config/context_processors.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from register import const as register_const
+
+
+def const_str(request):
+    """
+    HTMLメタデータを渡す
+    """
+    return {
+        # 性別
+        'GENDER_MALE': register_const.GENDER_MALE,
+        'GENDER_FEMALE': register_const.GENDER_FEMALE,
+        'GENDER_OTHER': register_const.GENDER_OTHER,
+        'GENDER_CHOICES': register_const.GENDER_CHOICES,
+
+        # ユーザー権限
+        'PRIVILEGE_ADMIN': register_const.PRIVILEGE_ADMIN,
+        'PRIVILEGE_EDITOR': register_const.PRIVILEGE_EDITOR,
+        'PRIVILEGE_REFERENCE': register_const.PRIVILEGE_REFERENCE,
+        'PRIVILEGE_GUEST': register_const.PRIVILEGE_GUEST,
+        'PRIVILEGE_CHOICES': register_const.PRIVILEGE_CHOICES,
+    }
