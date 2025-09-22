715f26bccd05b82ea198a09d07c29e4d8cb878bc
Kyosuke <love.frisk1188@gmail.com>
2025-09-22 15:39:09 +0900

    javascript部分を共通化

--- src/static/js/item_mst/script.js
+++ src/static/js/item_mst/script.js
@@ -34,48 +34,8 @@ $(function () {
     });
 
     // ファイル選択時
-    $("#file-input").on("change", function () {
-        var file = this.files[0];
-        if (!file) return;
-
-        var formData = new FormData();
-        formData.append('file', file);
-        formData.append('csrfmiddlewaretoken', $('[name=csrfmiddlewaretoken]').val());
-
-        // テンプレートから渡された data-url を取得
-        var url = $("#import-btn").data('action');
-
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: formData,
-            contentType: false,
-            processData: false,
-            xhr: function () {
-                var xhr = $.ajaxSettings.xhr();
-                if (xhr.upload) {
-                    xhr.upload.addEventListener("progress", function (e) {
-                        if (e.lengthComputable) {
-                            var percent = Math.round((e.loaded / e.total) * 100);
-                            $(".progress").show();
-                            $("#progress-bar").css("width", percent + "%").text(percent + "%");
-                        }
-                    }, false);
-                }
-                return xhr;
-            }
-        })
-        .done(function(response) {
-            alert("アップロード完了: " + response.message);
-            location.reload();
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            $(".progress-bar")
-                .removeClass("bg-success")
-                .addClass("bg-danger")
-                .text("Import時にエラー発生");
-            alert(['アップロード失敗:', jqXHR.responseJSON?.error || '',  jqXHR.responseJSON?.details || ''].join('\n'));
-        });
+    $('#file-input').on('change', function () {
+        $().import_data(this);
     });
 
     // モーダル画面を閉じた時の処理
