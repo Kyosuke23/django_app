6a01a717b95c7b4180a17cb4e22cede4081a6b0a
Kyosuke <love.frisk1188@gmail.com>
2025-09-29 13:19:08 +0900

    【取引先マスタ】リファクタリング

--- src/partner_mst/urls.py
+++ src/partner_mst/urls.py
@@ -7,13 +7,11 @@ urlpatterns = [
     # -----------------
     # 取引先マスタ
     # -----------------
-    path('partner/', views.PartnerListView.as_view(), name='partner_list'),
-    path('partner/create/', views.PartnerCreateView.as_view(), name='partner_create'),
-    path('partner/<int:pk>/update/', views.PartnerUpdateView.as_view(), name='partner_update'),
-    path('partner/<int:pk>/delete/', views.PartnerDeleteView.as_view(), name='partner_delete'),
+    path('partner/', views.PartnerListView.as_view(), name='list'),
+    path('partner/<int:pk>/delete/', views.PartnerDeleteView.as_view(), name='delete'),
     path("partner/import/csv", views.ImportCSV.as_view(), name='import_csv'),
     path('partner/export/csv', views.ExportCSV.as_view(), name='export_csv'),
     path('partner/export/excel', views.ExportExcel.as_view(), name='export_excel'),
-    path('partner/modal/create/', views.PartnerCreateModalView.as_view(), name='partner_create_modal'),
-    path('partner/<int:pk>/modal/update/', views.PartnerUpdateModalView.as_view(), name='partner_update_modal'),
+    path('partner/modal/create/', views.PartnerCreateView.as_view(), name='create'),
+    path('partner/<int:pk>/modal/update/', views.PartnerUpdateView.as_view(), name='update'),
 ]
--- src/partner_mst/views.py
+++ src/partner_mst/views.py
@@ -11,146 +11,143 @@ from django.template.loader import render_to_string
 
 # CSV/Excel の共通出力カラム定義
 DATA_COLUMNS = [
-    "partner_name", "contact_name",
-    "email", "tel_number", "postal_cd", "state",
-    "city", "address", "address2"
+    'partner_name', 'contact_name',
+    'email', 'tel_number', 'postal_cd', 'state',
+    'city', 'address', 'address2'
 ] + Common.COMMON_DATA_COLUMNS
 
+FILENAME_PREFIX = 'partner_mst'
 
 # -----------------------------
-# Partner CRUD (通常画面)
+# Partner CRUD
 # -----------------------------
 
 class PartnerListView(generic.ListView):
     model = Partner
-    template_name = "partner_mst/partner_list.html"
-    context_object_name = "partners"
+    template_name = 'partner_mst/list.html'
+    context_object_name = 'partners'
     paginate_by = 20
 
     def get_queryset(self):
-        queryset = Partner.objects.filter(is_deleted=False, tenant=self.request.user.tenant)
-
-        search = self.request.GET.get("search")
-
-        if search:
-            queryset = queryset.filter(
-                Q(partner_name__icontains=search) |
-                Q(contact_name__icontains=search) |
-                Q(email__icontains=search)
-            )
-
-        return queryset.order_by("partner_name")
+        query_set = Partner.objects.filter(is_deleted=False, tenant=self.request.user.tenant)
+        query_set = filter_data(request=self.request, query_set=query_set)
+        return query_set.order_by('partner_name')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["search"] = self.request.GET.get("search") or ""
+        context['search_keyword'] = self.request.GET.get('search_keyword') or ''
         context = Common.set_pagination(context, self.request.GET.urlencode())
         return context
 
 
 class PartnerCreateView(generic.CreateView):
+    '''
+    取引先登録モーダル版
+    - GET: 部分テンプレートを返す（Ajax）
+    - POST: Ajaxで登録処理
+    '''
     model = Partner
     form_class = PartnerForm
-    template_name = "partner_mst/partner_edit.html"
-    success_url = reverse_lazy("partner_mst:partner_list")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["is_update"] = False
-        return context
-
-    def form_valid(self, form):
-        form.instance.create_user = self.request.user
-        form.instance.update_user = self.request.user
-        form.instance.tenant = self.request.user.tenant
-        partner_message(self.request, "登録", form.instance.partner_name)
-        return super().form_valid(form)
-
-
-class PartnerUpdateView(generic.UpdateView):
-    model = Partner
-    form_class = PartnerForm
-    template_name = "partner_mst/partner_edit.html"
-    success_url = reverse_lazy("partner_mst:partner_list")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["is_update"] = True
-        return context
-
-    def form_valid(self, form):
-        form.instance.update_user = self.request.user
-        form.instance.tenant = self.request.user.tenant
-        response = super().form_valid(form)
-        partner_message(self.request, "更新", self.object.partner_name)
-        return response
-
-
-class PartnerDeleteView(generic.View):
-    model = Partner
-    template_name = "partner_mst/partner_confirm_delete.html"
-    success_url = reverse_lazy("partner_mst:partner_list")
-
-    def post(self, request, *args, **kwargs):
-        obj = Partner.objects.get(pk=kwargs["pk"])
-        obj.is_deleted = True
-        obj.update_user = request.user
-        obj.save()
-        partner_message(request, "削除", obj.partner_name)
-        return HttpResponseRedirect(reverse_lazy("partner_mst:partner_list"))
-
-
-# -----------------------------
-# Partner CRUD (モーダル画面)
-# -----------------------------
-
-class PartnerCreateModalView(PartnerCreateView):
-    template_name = "partner_mst/partner_form.html"
+    template_name = 'partner_mst/form.html'
 
     def get(self, request, *args, **kwargs):
         form = self.get_form()
         html = render_to_string(
             self.template_name,
-            {"form": form, "form_action": reverse("partner_mst:partner_create_modal"), "modal_title": "取引先新規登録"},
+            {
+                'form': form,
+                'form_action': reverse('partner_mst:create'),
+                'modal_title': '取引先新規登録',
+            },
             request
         )
-        return JsonResponse({"success": True, "html": html})
+        return JsonResponse({'success': True, 'html': html})
 
     def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.create_user = self.request.user
-        self.object.update_user = self.request.user
-        self.object.tenant = self.request.user.tenant
-        self.object.save()
-        partner_message(self.request, "登録", self.object.partner_name)
-        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
-            return JsonResponse({"success": True})
+        Common.save_data(selv=self, form=form, is_update=False)
+        partner_message(self.request, '登録', self.object.partner_name)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': True})
         return super().form_valid(form)
 
+    def form_invalid(self, form):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string(
+                self.template_name,
+                {
+                    'form': form,
+                    'form_action': reverse('partner_mst:create'),
+                    'modal_title': '取引先新規登録',
+                },
+                self.request
+            )
+            return JsonResponse({'success': False, 'html': html})
+        return super().form_invalid(form)
 
-class PartnerUpdateModalView(PartnerUpdateView):
-    template_name = "partner_mst/partner_form.html"
+
+class PartnerUpdateView(generic.UpdateView):
+    '''
+    取引先更新モーダル版
+    - GET: 部分テンプレートを返す（Ajax）
+    - POST: Ajaxで更新処理
+    '''
+    model = Partner
+    form_class = PartnerForm
+    template_name = 'partner_mst/form.html'
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         form = self.get_form()
         html = render_to_string(
             self.template_name,
-            {"form": form, "form_action": reverse("partner_mst:partner_update_modal", kwargs={"pk": self.object.pk}),
-             "modal_title": f"取引先更新: {self.object.partner_name}"},
+            {
+                'form': form,
+                'form_action': reverse('partner_mst:update', kwargs={'pk': self.object.pk}),
+                'modal_title': f'取引先更新: {self.object.partner_name}',
+            },
             request
         )
-        return JsonResponse({"success": True, "html": html})
+        return JsonResponse({'success': True, 'html': html})
 
     def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.update_user = self.request.user
-        self.object.tenant = self.request.user.tenant
-        self.object.save()
-        partner_message(self.request, "更新", self.object.partner_name)
-        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
-            return JsonResponse({"success": True})
+        Common.save_data(selv=self, form=form, is_update=True)
+        partner_message(self.request, '更新', self.object.partner_name)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': True})
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            obj = getattr(self, 'object', None)
+            modal_title = f'取引先更新: {obj.partner_name}' if obj else '取引先更新'
+            html = render_to_string(
+                self.template_name,
+                {
+                    'form': form,
+                    'form_action': reverse(
+                        'partner_mst:update',
+                        kwargs={'pk': obj.pk} if obj else {}
+                    ),
+                    'modal_title': modal_title,
+                },
+                self.request
+            )
+            return JsonResponse({'success': False, 'html': html})
+        return super().form_invalid(form)
+
+
+class PartnerDeleteView(generic.View):
+    model = Partner
+    template_name = 'partner_mst/confirm_delete.html'
+    success_url = reverse_lazy('partner_mst:list')
+
+    def post(self, request, *args, **kwargs):
+        obj = Partner.objects.get(pk=kwargs['pk'])
+        obj.is_deleted = True
+        obj.update_user = request.user
+        obj.save()
+        partner_message(request, '削除', obj.partner_name)
+        return HttpResponseRedirect(reverse_lazy('partner_mst:list'))
 
 
 # -----------------------------
@@ -159,12 +156,12 @@ class PartnerUpdateModalView(PartnerUpdateView):
 
 class ExportExcel(ExcelExportBaseView):
     model_class = Partner
-    filename_prefix = "partner_mst"
+    filename_prefix = FILENAME_PREFIX
     headers = DATA_COLUMNS
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return search_data(request=request, query_set=qs)
+        return filter_data(request=request, query_set=qs)
 
     def row(self, rec):
         return get_row(rec)
@@ -172,12 +169,12 @@ class ExportExcel(ExcelExportBaseView):
 
 class ExportCSV(CSVExportBaseView):
     model_class = Partner
-    filename_prefix = "partner_mst"
+    filename_prefix = FILENAME_PREFIX
     headers = DATA_COLUMNS
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return search_data(request=request, query_set=qs)
+        return filter_data(request=request, query_set=qs)
 
     def row(self, rec):
         return get_row(rec)
@@ -186,25 +183,25 @@ class ExportCSV(CSVExportBaseView):
 class ImportCSV(CSVImportBaseView):
     expected_headers = DATA_COLUMNS
     model_class = Partner
-    unique_field = "partner_name"
+    unique_field = FILENAME_PREFIX
 
     def validate_row(self, row, idx, existing, request):
-        partner_name = row.get("partner_name")
+        partner_name = row.get('partner_name')
         if not partner_name:
-            return None, f"{idx}行目: partner_name が空です"
+            return None, f'{idx}行目: partner_name が空です'
         if partner_name in existing:
-            return None, f"{idx}行目: partner_name '{partner_name}' は既に存在します"
+            return None, f'{idx}行目: partner_name "{partner_name}" は既に存在します'
 
         obj = Partner(
             partner_name=partner_name,
-            contact_name=row.get("contact_name"),
-            email=row.get("email"),
-            tel_number=row.get("tel_number"),
-            postal_cd=row.get("postal_cd"),
-            state=row.get("state"),
-            city=row.get("city"),
-            address=row.get("address"),
-            address2=row.get("address2"),
+            contact_name=row.get('contact_name'),
+            email=row.get('email'),
+            tel_number=row.get('tel_number'),
+            postal_cd=row.get('postal_cd'),
+            state=row.get('state'),
+            city=row.get('city'),
+            address=row.get('address'),
+            address2=row.get('address2'),
             create_user=request.user,
             update_user=request.user,
             tenant=request.user.tenant
@@ -230,8 +227,8 @@ def get_row(rec):
     ] + Common.get_common_columns(rec=rec)
 
 
-def search_data(request, query_set):
-    keyword = request.GET.get("search") or ""
+def filter_data(request, query_set):
+    keyword = request.GET.get('search_keyword') or ''
     if keyword:
         query_set = query_set.filter(
             Q(partner_name__icontains=keyword) |
@@ -242,4 +239,4 @@ def search_data(request, query_set):
 
 
 def partner_message(request, action, partner_name):
-    messages.success(request, f"取引先「{partner_name}」を{action}しました。")
+    messages.success(request, f'取引先「{partner_name}」を{action}しました。')
